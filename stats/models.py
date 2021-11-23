87099607e67006ec3cfb09b153157d611fe37add
Diana Wong <dwa102@sfu.ca>
2021-11-23 14:17:05 -0800

    added to match class

--- stats/models.py
+++ stats/models.py
@@ -6,6 +6,11 @@ from django.utils.text import slugify
 class Company(models.Model):
     name = models.CharField(max_length=100)
 
+class Player(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name =  models.CharField(max_length=50)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
 class Game(models.Model):
     title = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
@@ -17,12 +22,19 @@ class Game(models.Model):
         return super(Game, self).save(*args, **kwargs)
 
 class Match(models.Model):
-    pass
+    participant_A = models.ForeignKey(Player, on_delete=models.CASCADE)
+    participant_B = models.ForeignKey(Player, on_delete=models.CASCADE)
 
-class Results(models.Model):
-    date_posted = models.DateTimeField(default=timezone.now)
-    score = models.BooleanField
-    match = models.ForeignKey(Match, on_delete=models.CASCADE, null = True)
+    score_A = models.IntegerField(max_length=10)
+    score_B = models.IntegerField(max_length=10)
+    elo_change = models.IntegerField(max_length=20)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    match_date = models.DateTimeField(default=timezone.now)
+
+# class Results(models.Model):
+#     date_posted = models.DateTimeField(default=timezone.now)
+#     score = models.BooleanField
+#     match = models.ForeignKey(Match, on_delete=models.CASCADE, null = True)
 
 # class GamePlayer(models.Model):
 #     game = models.ForeignKey(Game, on_delete=models.CASCADE)
